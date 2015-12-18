e9f71a380fb7dda282b76337582e1fa056af7c9a
Pedro Fraca Tarancon <pedro.fraca@gmail.com>
2015-12-18 09:38:29 +0100

    Initial commit

---
+++ app/src/test/java/io/github/pedrofraca/leancodeandroid/ExampleUnitTest.java
@@ -0,0 +1,15 @@
+package io.github.pedrofraca.leancodeandroid;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(5, 2 + 2);
+    }
+}
